f36ab95708633bfc6cfc73545d84c7f2ee2c84f4
Eric Song <e7song@ucsd.edu>
2025-05-13 05:23:52 -0700

    fixed recipe array push

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -97,7 +97,7 @@ function initFormHandler() {
 		// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
 		//            then save the recipes array back to localStorage
 		const recipeArray = getRecipesFromStorage();
-		recipeArray.push(newRecipe);
+		recipeArray.push(recipeObject);
 		saveRecipesToStorage(recipeArray);
 
 	});
